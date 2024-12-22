820f7434336886240c067f02a48408dae9bc3c2b
Likhon22 <likhonsarker793@gmail.com>
2024-12-22 18:15:15 +0600

    project updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,14 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import routes from "./app/routes";
+import path from "path";
 
 import globalErrorHandler from "./app/middleware/globalErrorHandler";
 import notFound from "./app/middleware/notFound";
+import config from "./app/config";
+
 const app: Application = express();
+const __dirname = path.resolve();
 
 // middlewares
 app.use(express.json({ limit: "1mb" }));
@@ -22,6 +26,12 @@ app.use(cookieParser());
 // routes
 
 app.use("/api/v1", routes);
+if (config.node_env === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  });
+}
 
 const test = (req: Request, res: Response) => {
   res.send("Hello World!");
